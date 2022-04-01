06253563ff71c8774d114df0bdb6b7b90d3eb03d
Danielly Garcia Jardim <danielly.jardim@softfocus.com.br>
2022-04-01 13:16:16 -0300

    Fazendo refatoração do service, sem quebrar os testes

--- tdd/src/main/java/com/praticando/tdd/enums/FormaDePagamento.java
+++ tdd/src/main/java/com/praticando/tdd/enums/FormaDePagamento.java
@@ -2,8 +2,23 @@ package com.praticando.tdd.enums;
 
 public enum FormaDePagamento {
 
-    DINHEIRO,
-    DEBITO,
-    CREDITO
+    DINHEIRO {
+        public double percentualDesconto() {
+            return 0.1;
+        }
+    },
 
+    DEBITO {
+        public double percentualDesconto() {
+            return 0.06;
+        }
+    },
+
+    CREDITO {
+        public double percentualDesconto() {
+            return 0;
+        }
+    };
+
+    public abstract double percentualDesconto();
 }
--- tdd/src/main/java/com/praticando/tdd/service/DescontoService.java
+++ tdd/src/main/java/com/praticando/tdd/service/DescontoService.java
@@ -6,11 +6,7 @@ import com.praticando.tdd.enums.FormaDePagamento;
 public class DescontoService {
 
     public double calcularDesconto(double valorDaCompra, FormaDePagamento formaDePagamento) {
-        if (formaDePagamento == FormaDePagamento.DINHEIRO) {
-            return valorDaCompra * 0.1;
-        }  else if (formaDePagamento == FormaDePagamento.DEBITO) {
-            return valorDaCompra * 0.06;
-        }
-        return 0;
+        return valorDaCompra * formaDePagamento.percentualDesconto();
     }
+
 }
