7267e50e4d0fcd184637883dfe492ac951e0c868
Danielly Garcia Jardim <danielly.jardim@softfocus.com.br>
2022-04-01 11:33:03 -0300

    Criando novo teste

--- tdd/src/test/java/com/praticando/tdd/service/DescontoServiceTest.java
+++ tdd/src/test/java/com/praticando/tdd/service/DescontoServiceTest.java
@@ -16,11 +16,18 @@ public class DescontoServiceTest {
     }
 
     @Test
-
     void deveConcederDescontoQuandoPagamentoAVistaNoDebito() {
         DescontoService descontoService = new DescontoService();
         double desconto = descontoService.calcularDesconto( 1000.00, FormaDePagamento.DEBITO);
         double descontoEsperado = 60.00;
+        Assertions.assertThat(desconto).isEqualTo(descontoEsperado);
+    }
+
+    @Test
+    void naoDeveConcederDescontoQuandoPagamentoNoCredito() {
+        DescontoService descontoService = new DescontoService();
+        double desconto = descontoService.calcularDesconto( 1000.00, FormaDePagamento.CREDITO);
+        double descontoEsperado = 0.00;
         Assertions.assertThat(desconto).isEqualTo(descontoEsperado);
     }
 }
